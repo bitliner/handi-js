c40c79693c234cc7dcaed759541b08f32f7d0cf5
Giovanni Gaglione <giovanni@wonderflow.ai>
2024-08-21 00:46:20 +0200

    feat(app): added GET /form, building programmatically a static tailwind form

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 import fastifyView from '@fastify/view'
 import fastify from 'fastify'
 import ejs from 'ejs'
+import path from 'node:path'
+import { fileURLToPath } from 'url'
+import fastifyStatic from '@fastify/static'
+import { buildHtmlFile, BUILT_CSS_FILE, RAW_EJS_FILENAME } from './lib/build-tailwind.js'
+import log from './lib/log.js'
+
+const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
+const __dirname = path.dirname(__filename)
 
 export function buildApp () {
   const app = fastify()
+
+  app.register(fastifyStatic, {
+    root: path.join(__dirname, 'css'),
+    prefix: '/css/' // optional: default '/'
+  })
 
   app.register(fastifyView, {
     engine: {
@@ -12,12 +25,16 @@ export function buildApp () {
     root: './views'
   })
 
-  app.get('/', (req, reply) => {
-    reply.view('index.ejs', { name: 'User' })
+  app.get('/form', async (req, reply) => {
+    await buildHtmlFile()
+
+    log.info(`serving ${RAW_EJS_FILENAME} with cssPath=${BUILT_CSS_FILE}`)
+
+    return reply.view(RAW_EJS_FILENAME, { cssPath: BUILT_CSS_FILE })
   })
 
-  app.post('/form', (req, reply) => {
-    reply.view('form.ejs', { name: 'User' })
+  app.get('/', (req, reply) => {
+    return reply.view('index.ejs', { name: 'User2' })
   })
 
   return app
