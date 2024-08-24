96e0db81cc2ddaa7ebd60086c8611ea19b08db08
Giovanni Gaglione <giovanni@wonderflow.ai>
2024-08-24 17:01:03 +0200

    feat(daisy): configured daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-module.exports = {
-  content: ['./lib/components/*.{js,jsx,ts,tsx}'],
+import daisyUi from 'daisyui'
+
+export default {
+  // content: ['./lib/components/*.{js,jsx,ts,tsx}'],
   // Toggle dark-mode based on .dark class or data-mode="dark"
-  darkMode: ['class', '[data-mode="dark"]'],
+  // darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [daisyUi]
 }
